243cca7f490026c73ec45cf77807382aa072523e
ogzhanolguncu <ogzhan11@gmail.com>
2023-09-09 22:57:39 +0300

    Add ability to check expirations with a thread

--- src/store/db.rs
+++ src/store/db.rs
@@ -1,37 +1,63 @@
-    use std::collections::HashMap;
-    use std::sync::{Arc, RwLock};
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+use std::thread;
+use std::time::{Duration, Instant};
 
-    #[derive(Debug, Clone)]
-    pub struct Cache {
-        data: Arc<RwLock<HashMap<String, String>>>,
-    }
+#[derive(Debug, Clone)]
+pub struct Cache {
+    data: Arc<RwLock<HashMap<String, String>>>,
+    expirations: Arc<RwLock<HashMap<String, Instant>>>,
+}
 
-    impl Cache {
-        pub fn new() -> Self {
-            Self {
-                data: Arc::new(RwLock::new(HashMap::new())),
+impl Cache {
+    pub fn new() -> Self {
+        let data = Arc::new(RwLock::new(HashMap::new()));
+        let expirations = Arc::new(RwLock::new(HashMap::new()));
+        let cache = Self { data, expirations };
+
+        // Spawns a thread that runs every sec to check expirations hashmap to control if there are any expired key-val pairs.
+        let data_clone = cache.data.clone();
+        let expirations_clone = cache.expirations.clone();
+        thread::spawn(move || loop {
+            let mut data = data_clone.write().unwrap();
+            let expirations = expirations_clone.read().unwrap();
+            let now = Instant::now();
+            for (key, &time) in expirations.iter() {
+                if time <= now {
+                    data.remove(key);
+                }
             }
-        }
-
-        pub fn set(&self, key: String, value: String) {
-            let mut data = self.data.write().unwrap();
-            data.insert(key, value);
-        }
-
-        pub fn get(&self, key: &str) -> Option<String> {
-            let data = self.data.read().unwrap();
-            data.get(key).cloned()
-        }
+            thread::sleep(Duration::from_secs(1));
+        });
+        cache
     }
 
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn should_initialize_and_get_set() {
-            let cache = Cache::new();
-            cache.set(String::from("name"), String::from("The Wizard of Oz"));
-            assert_eq!("The Wizard of Oz", cache.get("name").unwrap());
-        }
+    pub fn set_with_seconds(&self, key: String, value: String, secs: Duration) {
+        let mut data = self.data.write().unwrap();
+        let mut expirations = self.expirations.write().unwrap();
+        data.insert(key.clone(), value);
+        expirations.insert(key, Instant::now() + secs);
     }
+
+    pub fn set(&self, key: String, value: String) {
+        let mut data = self.data.write().unwrap();
+        data.insert(key, value);
+    }
+
+    pub fn get(&self, key: &str) -> Option<String> {
+        let data = self.data.read().unwrap();
+        data.get(key).cloned()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_initialize_and_get_set() {
+        let cache = Cache::new();
+        cache.set(String::from("name"), String::from("The Wizard of Oz"));
+        assert_eq!("The Wizard of Oz", cache.get("name").unwrap());
+    }
+}
